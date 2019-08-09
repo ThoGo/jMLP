5b70f16ca4112ebe314b14653bc494cc50d8caf8
omm <thomas.goettsche@gmx.de>
2019-08-09 15:17:19 +0200

    [jMLP] Neuron does feedForward()

--- src/jMLP/Neuron.java
+++ src/jMLP/Neuron.java
@@ -25,6 +25,21 @@ public class Neuron
 			inputWeights.add( rand.nextFloat() );
 		}
 	}
+	
+	public Float feedForward(List<Float> inputs)
+	{
+		//XXX Aussteigen, wenn die Dimensionen nicht stimmen
+		
+		Float output = 0F;
+		
+		for( int i=0;  i<inputWeights.size(); i++ )
+		{
+			//entspricht einer linearen Transferfunktion
+			output += inputWeights.get(i) * inputs.get(i);
+		}
+		
+		return output;
+	}
 
 
 //	public void adapt()
@@ -52,5 +67,12 @@ public class Neuron
 		// weights sind aber auf jeden Fall initialisiert
 		return this.inputWeights.size();
 	}
+
+	public List<Float> getInputWeights()
+	{
+		return inputWeights;
+	}
+	
+	
 
 }
--- test/jMLP/NeuronTest.java
+++ test/jMLP/NeuronTest.java
@@ -2,6 +2,9 @@ package jMLP;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 class NeuronTest
@@ -18,6 +21,23 @@ class NeuronTest
 		neuron = new Neuron(15);
 		neuron.printWeights();
 		assertEquals( 15, neuron.getInputSize());
+	}
+	
+	@Test
+	void testFeedForward()
+	{
+		Neuron neuron;
+		
+		//trivialer Test: Neuron mit einem Eingang
+		// und der input ist 1 => output == weight (wenn die Transferfunktion "id()" ist.)
+		neuron = new Neuron(1);
+		neuron.printWeights();
+		Float weight = neuron.getInputWeights().get(0);
+
+		List<Float> inputs = Arrays.asList(1F);
+		Float output = neuron.feedForward(inputs);
+		
+		assertEquals( weight, output);
 	}
 
 }
